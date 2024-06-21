4708e222da3422a822bfe6818417cce999f338c9
Daniel Levi <daniellevi000@gmaiil.com>
2024-06-21 15:29:55 -0300

    estrutura inicial

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import fastify from 'fastify'
+import { app } from './app'
+
+
+
+const PORT =  3333
+app.listen({
+    port: PORT,
+    host: '0.0.0.0'
+}, () => {
+  console.log(`🚀 server run listening on Port ${PORT} `)
+})

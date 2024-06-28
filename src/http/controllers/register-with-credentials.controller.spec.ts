edbf41ed0090122824b68cece765162df8d8b046
SDaniel Levi <daniellevi000@gmail.com>
2024-06-28 13:23:34 -0300

    feat:craete test e2e register

--- src/http/controllers/register-with-credentials.controller.spec.ts
+++ src/http/controllers/register-with-credentials.controller.spec.ts
@@ -1,3 +1,22 @@
-test('register with credentials', () => {
-  expect(true).toBe(true)
+import request from 'supertest'
+import { app } from '@/app'
+
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      username: 'johndoe',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
 })

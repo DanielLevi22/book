d31c75af80f3d2288b1ed92982fc2e5c9bc690c7
SDaniel Levi <daniellevi000@gmail.com>
2024-07-02 18:48:52 -0300

    feat: create test get popular books

---
+++ src/http/controllers/book/get-popular-books.spec.ts
@@ -0,0 +1,18 @@
+import request from 'supertest'
+import { app } from '@/app'
+
+describe('Get popular books (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to get recent reviews', async () => {
+    const response = await request(app.server).get('/popular-books').send()
+    console.log('response', response.body)
+    expect(response.statusCode).toEqual(200)
+  })
+})
